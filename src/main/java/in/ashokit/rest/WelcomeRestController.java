bba438b246a0e39a15c883166d676b548f582f07
Devi Vara Prasad Reddy Kadimisetti <devivaraprasadreddy2210@gmail.com>
2024-08-09 19:31:23 +0530

    spring project

---
+++ src/main/java/in/ashokit/rest/WelcomeRestController.java
@@ -0,0 +1,25 @@
+package in.ashokit.rest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeRestController {
+
+	private Logger logger = LoggerFactory.getLogger(WelcomeRestController.class);
+
+	public WelcomeRestController() {
+		logger.info("***** WelcomeRestController::Constructor *****");
+	}
+
+	@GetMapping
+	public String welcomeMsg() {
+		logger.info("***** welcomeMsg() execution start *****");
+		String msg = "Welcome..!! Devi Vara Prasad Reddy DevOps Class...";
+		logger.info("***** welcomeMsg() execution end *****");
+		return msg;
+	}
+}
